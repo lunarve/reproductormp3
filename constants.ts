a85de55cf60aa5048e0118355da008fc8ef030ad
lunarve <nomad.design.arg@hotmail.com>
2025-10-19 09:49:24 -0300

    Create constants.ts

---
+++ constants.ts
@@ -0,0 +1,22 @@
+export const tracks = [
+  {
+    title: "Bajo las Cenizas, el Fuego",
+    src: "/media/Bajo%20las%20Cenizas,%20el%20Fuego.mp3",
+  },
+  {
+    title: "Eco de la Tierra",
+    src: "/media/Eco%20de%20la%20Tierra.mp3",
+  },
+  {
+    title: "El Silencio Tras el Nombre",
+    src: "/media/El%20Silencio%20Tras%20el%20Nombre.mp3",
+  },
+  {
+    title: "Eres el Camino",
+    src: "/media/Eres%20el%20Camino.mp3",
+  },
+  {
+    title: "Pulso del Ser",
+    src: "/media/Pulso%20del%20Ser.mp3",
+  },
+];
