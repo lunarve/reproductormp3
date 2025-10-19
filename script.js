d04ff7aba4367657f2d4af7759407e99ba4e386a
lunarve <nomad.design.arg@hotmail.com>
2025-10-19 09:58:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const tracks = [
   {
     title: "Bajo las Cenizas, el Fuego",
-    src: "media/Bajo%20las%20Cenizas,%20el%20Fuego.mp3",
+    src: "public/media/Bajo%20las%20Cenizas,%20el%20Fuego.mp3",
   },
   {
     title: "Eco de la Tierra",
-    src: "media/Eco%20de%20la%20Tierra.mp3",
+    src: "public/media/Eco%20de%20la%20Tierra.mp3",
   },
   {
     title: "El Silencio Tras el Nombre",
-    src: "media/El%20Silencio%20Tras%20el%20Nombre.mp3",
+    src: "public/media/El%20Silencio%20Tras%20el%20Nombre.mp3",
   },
   {
     title: "Eres el Camino",
-    src: "media/Eres%20el%20Camino.mp3",
+    src: "public/media/Eres%20el%20Camino.mp3",
   },
   {
     title: "Pulso del Ser",
-    src: "media/Pulso%20del%20Ser.mp3",
+    src: "public/media/Pulso%20del%20Ser.mp3",
   },
 ];
 
